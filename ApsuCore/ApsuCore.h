95ecd0d3c4b81faa6eebbc785b7d29db0005f907
David Moles <chronodm@gmail.com>
2014-07-04 12:56:35 -0700

    Initial Commit

---
+++ ApsuCore/ApsuCore.h
@@ -0,0 +1,19 @@
+//
+//  ApsuCore.h
+//  ApsuCore
+//
+//  Created by David Moles on 7/4/14.
+//  Copyright (c) 2014 David Moles. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ApsuCore.
+FOUNDATION_EXPORT double ApsuCoreVersionNumber;
+
+//! Project version string for ApsuCore.
+FOUNDATION_EXPORT const unsigned char ApsuCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApsuCore/PublicHeader.h>
+
+
